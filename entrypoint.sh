4c780f0a4a7637304468d0a53ef8414affb776ee
Chris Thomas <christopher.thomas@plista.com>
2019-04-24 13:03:47 +0200

    parameterise the config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+function required () {
+    eval v="\$$1";
+
+    if [ -z "$v" ]; then
+        echo "$1 envvar is not configured, exiting..."
+        exit 0;
+    else
+        [ ! -z "${ENTRYPOINT_DEBUG}" ] && echo "Rewriting required variable '$1' in file '$2'"
+        sed -i "s~{{ $1 }}~$v~g" $2
+    fi
+}
+
+function optional () {
+    eval v="\$$1";
+
+    [ ! -z "${ENTRYPOINT_DEBUG}" ] && echo "Rewriting optional variable '$1' in file '$2'"
+    sed -i "s~{{ $1 }}~$v~g" $2
+}
+
+for file in $(find /etc/clamav -type f); do
+    required CLAMAV_PORT ${file}
+done
+
 # Bootstrap the database if clamav is running for the first time
 [ -f /data/main.cvd ] || freshclam
 
